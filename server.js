4a1b03e561f0b541b9665d4b50567c39599e5b4d
dev-algarrobo <sebastian1377@gmail.com>
2026-05-25 12:12:28 -0400

    chore: Eliminacion de codigo muerto y dependencias de backup (xlsx, googleapis)

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ import logsRouter from "./routes/logs.js";
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-// export const filePath = __dirname+'/client/public/exportdata.xlsx'
-// export const filePath = __dirname+'/tmp/exportdata.xlsx'
-export const filePath = "/tmp/exportdata.xlsx";
 dotenv.config();
 
 app.use(helmet());
